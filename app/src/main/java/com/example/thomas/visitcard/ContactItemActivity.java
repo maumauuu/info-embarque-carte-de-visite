6996d5f740ea35310153383674530f00e0cb7bdf
06tom06 <thomas.suignard.ts@gmail.com>
2017-03-16 08:51:11 +0100

    On peut maintenant voir les info de notre contact en detail

--- app/src/main/java/com/example/thomas/visitcard/ContactItemActivity.java
+++ app/src/main/java/com/example/thomas/visitcard/ContactItemActivity.java
@@ -1,5 +1,6 @@
 package com.example.thomas.visitcard;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -13,5 +14,14 @@ public class ContactItemActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_item);
+
+        Intent i = getIntent();
+
+        tvEmail = (TextView) findViewById(R.id.tvEmail);
+        tvNum = (TextView) findViewById(R.id.tvNumero);
+
+        tvEmail.setText(i.getStringExtra("email"));
+        tvNum.setText(i.getStringExtra("num"));
+
     }
 }
