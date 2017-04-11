fe729bbf59c5407aa6110405fa0143b2f1c8772e
06tom06 <thomas.suignard.ts@gmail.com>
2017-04-11 09:55:54 +0200

    correction du scan

--- app/src/main/java/com/CDV/MainActivity.java
+++ app/src/main/java/com/CDV/MainActivity.java
@@ -1,5 +1,6 @@
 package com.CDV;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.Bundle;
@@ -10,14 +11,27 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private  String name;
+    private  String Lastname;
+    private  String Email;
+    private  String phone;
+    private  String Address;
+    private  String City;
+    private  String Postal;
+
+    private CarteDataSource datasource;
 
     private Toolbar toolbar;
     private TabLayout tabLayout;
@@ -38,6 +52,9 @@ public class MainActivity extends AppCompatActivity {
 
         tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
+
+        datasource = new CarteDataSource(this);
+        datasource.open();
     }
 
     public void scanner(View view){
@@ -84,5 +101,40 @@ public class MainActivity extends AppCompatActivity {
         public CharSequence getPageTitle(int position) {
             return mFragmentTitleList.get(position);
         }
+    }
+
+    protected void Fill(IntentResult result){
+        String c = result.getContents();
+        name = "ffff";
+        Lastname ="ggggg" ;
+        Email = "ffffsdsd";
+        phone = "22364126";
+        Address = "hlglfmg";
+        City = "rfdff" ;
+        Postal = "26634";
+
+        datasource.createCarte(name, Lastname,Email,phone,Address,City,Postal);
+
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result= IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if(result!=null){
+            if(result.getContents()==null){
+                Log.d("Main", "Cancelled scan");
+                //  Toast.makeText(this, "Cancelled: ", Toast.LENGTH_SHORT).show();
+            } else{
+                Log.d("Main", "Scanned" + result.getContents());
+                Log.d("Main", "Scanned" + result.getFormatName());
+                // Toast.makeText(this, "Scanned: "+ result.getContents(), Toast.LENGTH_SHORT).show();
+                Fill(result);
+                startActivity(new Intent(MainActivity.this, MainActivity.class));
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+            startActivity(new Intent(MainActivity.this, MainActivity.class));
+        }
+
     }
 }
