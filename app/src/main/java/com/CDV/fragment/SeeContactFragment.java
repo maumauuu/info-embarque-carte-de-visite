5de118b6f1a7480715f29c24bf79f779313e884d
maumau <maurgene.ghezal@gmail.com>
2017-04-23 21:34:33 +0200

    ...

--- app/src/main/java/com/CDV/fragment/SeeContactFragment.java
+++ app/src/main/java/com/CDV/fragment/SeeContactFragment.java
@@ -1,30 +1,12 @@
 package com.CDV.fragment;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.ListFragment;
-import android.support.v4.app.LoaderManager.LoaderCallbacks;
-import android.support.v4.content.CursorLoader;
-import android.support.v4.content.Loader;
-import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ListView;
 
 import com.CDV.R;
-
-import static com.CDV.util.util.retrieveContacts;
-
-import java.util.List;
-
 
 public class SeeContactFragment extends Fragment {
 
