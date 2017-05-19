65e3c1f26754ce8b7eafdd455437420384d136e8
maumau <maurgene.ghezal@gmail.com>
2017-05-19 15:18:22 +0200

    #4 On peut ajouté le contact une fois le sms lu

--- app/src/main/java/com/CDV/CarteContact.java
+++ app/src/main/java/com/CDV/CarteContact.java
@@ -1,12 +1,25 @@
 package com.CDV;
 
 import android.app.ActionBar;
+import android.content.ContentProviderOperation;
+import android.content.ContentProviderResult;
 import android.content.Intent;
+import android.content.OperationApplicationException;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.CDV.R;
+
+import java.util.ArrayList;
+
+import static android.provider.ContactsContract.CommonDataKinds.Phone.TYPE_WORK;
 
 
 public class CarteContact extends AppCompatActivity {
@@ -28,32 +41,87 @@ public class CarteContact extends AppCompatActivity {
         setContentView(R.layout.activity_carte_contact);
 
         i = getIntent();
-
-        origin =i.getStringExtra("origin");
-
-
-        if(origin == "SMS"){
-            msg = i.getStringExtra("msg");
-            msgs = msg.split("\n");
-            name= msgs[1];
-            mail = msgs[2];
-            adresse = msgs[3];
-            phone = i.getStringExtra("phone");
+        msg = i.getStringExtra("msg");
+        msgs = msg.split("\n");
+        Log.d("00000000000", msgs[0]);
+        name= msgs[1];
+        mail = msgs[2];
+        adresse = msgs[3];
+        phone = i.getStringExtra("phone");
 
 
-
-        }else {
-
-            name = i.getStringExtra("name");
-            phone = i.getStringExtra("phone");
-            mail = i.getStringExtra("email");
-            adresse = i.getStringExtra("address");
-        }
         ((TextView) findViewById(R.id.Name)).setText(name);
         ((TextView) findViewById(R.id.phone)).setText(phone);
         ((TextView) findViewById(R.id.email)).setText(mail);
         ((TextView) findViewById(R.id.adresse)).setText(adresse);
 
     }
+
+    //ajoute le contact dans la BD du tel
+    public void AddContact(View view){
+        ArrayList<ContentProviderOperation> ops = new ArrayList<ContentProviderOperation>();
+        int rawContactInsertIndex = ops.size();
+
+        ops.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null).build());
+
+        //Phone Number
+        ops.add(ContentProviderOperation
+                .newInsert(ContactsContract.Data.CONTENT_URI)
+                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID,
+                        rawContactInsertIndex)
+                    .withValue(ContactsContract.Contacts.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, phone)
+                    .withValue(ContactsContract.Contacts.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.Phone.TYPE, TYPE_WORK).build());
+
+            //Display name/Contact name
+            ops.add(ContentProviderOperation
+                    .newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.Contacts.Data.RAW_CONTACT_ID,
+                            rawContactInsertIndex)
+                    .withValue(ContactsContract.Contacts.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, name)
+                    .build());
+            //Email details
+            ops.add(ContentProviderOperation
+                    .newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID,
+                            rawContactInsertIndex)
+                    .withValue(ContactsContract.Contacts.Data.MIMETYPE, ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.Email.DATA, mail)
+                    .withValue(ContactsContract.Contacts.Data.MIMETYPE, ContactsContract.CommonDataKinds.Email.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.Email.TYPE, "2").build());
+
+
+            //Postal Address
+            ops.add(ContentProviderOperation
+                    .newInsert(ContactsContract.Data.CONTENT_URI)
+                    .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID,
+                            rawContactInsertIndex)
+
+                    .withValue(ContactsContract.Contacts.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.StructuredPostal.DATA,adresse)
+
+                    .withValue(ContactsContract.Contacts.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredPostal.CONTENT_ITEM_TYPE)
+                    .withValue(ContactsContract.CommonDataKinds.StructuredPostal.TYPE, "2")
+
+                    .build());
+            try {
+                ContentProviderResult[] res = this.getContentResolver().applyBatch(
+                        ContactsContract.AUTHORITY, ops);
+            } catch (RemoteException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (OperationApplicationException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        Toast.makeText(this, "Contact ajouté", Toast.LENGTH_SHORT).show();
+
+
+    }
+
     
 }
