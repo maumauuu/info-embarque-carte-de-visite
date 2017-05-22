eb63fe995d2d110fbc1634f5aae32ac52f316493
maumau <maurgene.ghezal@gmail.com>
2017-05-22 13:49:46 +0200

    #10 #11 #16 ajout de MainActivityTest

--- app/src/androidTest/java/com/CDV/MainActivityTest.java
+++ app/src/androidTest/java/com/CDV/MainActivityTest.java
@@ -4,14 +4,21 @@ package com.CDV;
 import android.support.test.InstrumentationRegistry;
 import android.test.ActivityInstrumentationTestCase2;
 
+import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.Before;
 
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 
+
+@RunWith(AndroidJUnit4.class)
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity> {
 
 
@@ -24,17 +31,55 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     @Before
     public void setup()  throws Exception {
         super.setUp();
-        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+       injectInstrumentation(InstrumentationRegistry.getInstrumentation());
         getActivity();
     }
 
     @Test
     public void testInitial() {
-    //    fragment.getView().findViewById();
-      //  fragment.getView().findViewById(Profil);
+        onView(withId(R.id.layoutcdv))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.layoutgenerer))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.layoutSend))
+                .check(matches(isDisplayed()));
+
 
     }
 
+    @Test
+    public void testClickLayoutcdv() {
+        onView(withId(R.id.layoutcdv))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.layoutcdv))
+                .perform(click());
+        onView(withId(R.id.cdv))
+                .check(matches(isDisplayed()));
+
+    }
+
+    @Test
+    @Ignore
+    public void testClickLayoutsend() {
+        onView(withId(R.id.layoutSend))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.layoutSend))
+                .perform(click());
+        onView(withId(R.id.Send))
+                .check(matches(isDisplayed()));
+
+    }
+
+    @Test
+    public void testClickLayoutgenerer() {
+        onView(withId(R.id.layoutgenerer))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.layoutgenerer))
+                .perform(click());
+        onView(withId(R.id.qr_code))
+                .check(matches(isDisplayed()));
+
+    }
 
 
 }
