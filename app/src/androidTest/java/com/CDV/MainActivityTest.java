a51e8cd942719d2bf629dc84ae26aee9dd2f153f
06tom06 <thomas.suignard.ts@gmail.com>
2017-10-24 12:09:49 +0200

    correction

--- app/src/androidTest/java/com/CDV/MainActivityTest.java
+++ app/src/androidTest/java/com/CDV/MainActivityTest.java
@@ -3,7 +3,6 @@ package com.CDV;
 
 import android.support.test.InstrumentationRegistry;
 import android.test.ActivityInstrumentationTestCase2;
-
 import android.support.test.runner.AndroidJUnit4;
 
 import com.CDV.fragment.SeeContactFragment;
